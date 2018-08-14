6ff793caa14dec0e82abcc795d5bdced7d2e31fe
Olena Stotska <i.olena.stotska@gmail.com>
2018-08-14 09:29:02 +0300

    feat(cards): generate cards array

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Memory Game</h1>
+    <GameBoard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import GameBoard from './components/GameBoard';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld,
+    GameBoard,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-size: 14px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="game-area">
+    <div v-for="(cell, index) in cells" :key="index">
+      <div class="cell">
+        <img :src="cell">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: () => ({
+    cells: [],
+  }),
+  methods: {
+    genCards() {
+      for (let i = 0; i < 18; i++) {
+        this.cells.push(require(`@/assets/img/image${i + 1}.jpg`));
+      }
+    },
+  },
+
+  created() {
+    this.genCards();
+    this.genCards();
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.game-area {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  position: relative;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.cell {
+  width: 6rem;
+  height: 6rem;
+  margin: 10px;
+  border-radius: 10px;
+  background-color: #4b258a;
+}
+</style>
